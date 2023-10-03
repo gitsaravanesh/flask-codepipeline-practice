7c7bccbf96792a18025ec558807d80e58f80ba0c
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-10-03 09:03:46 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,5 +5,4 @@ cd /var/www/myapp
 # Install Python dependencies
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
-sudo pip3 install --upgrade pip
-sudo pip3 install --ignore-installed Flask
+pip3 install -r requirements.txt
