d7209c7c1bea3b4bb01d81ebcca3bcd97c15f9a4
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-10-03 09:04:22 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,8 +2,5 @@
 ls
 pwd
 cd /var/www/myapp
-
 # Start the Flask application server
-export FLASK_APP=app.py
-export FLASK_ENV=production
-flask run --host=0.0.0.0 --port=5000
+gunicorn -b 0.0.0.0:5000 app:app > /var/www/myapp/logs/gunicorn.log 2>&1 &
