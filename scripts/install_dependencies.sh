e0de9007a56774c8f3154c18ced7922114115c09
Saravanesh <126149128+gitsaravanesh@users.noreply.github.com>
2024-12-16 18:33:34 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,4 +5,5 @@ cd /var/www/myapp
 # Install Python dependencies
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
+pip install "flask>=2.0,<2.1" "werkzeug>=2.0,<2.1"
 pip3 install -r requirements.txt
