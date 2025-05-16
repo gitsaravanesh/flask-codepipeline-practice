ef0f05a9a79ea5266a541272a67e76948f279f9a
Saravanesh <126149128+gitsaravanesh@users.noreply.github.com>
2025-05-16 18:15:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -xe
 
 cd /var/www/myapp
 
-# Install Python dependencies
+# Install Python and virtualenv support
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip
-pip install "flask>=2.0,<2.1" "werkzeug>=2.0,<2.1"
-pip3 install -r requirements.txt
+sudo apt-get install -y python3 python3-pip python3-venv
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
