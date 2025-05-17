daf7f8d054a266e018676f170f79a69d8c49e227
Saravanesh <126149128+gitsaravanesh@users.noreply.github.com>
2025-05-17 16:51:21 +0530

    Color Change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    colors = ['yellow', 'red', 'blue', 'green', 'red', 'blue', 'violet']
+    colors = ['yellow', 'yellow', 'blue', 'green', 'red', 'blue', 'violet']
     return render_template('index.html', colors=colors)
 
 if __name__ == '__main__':
